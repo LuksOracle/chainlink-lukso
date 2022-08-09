ca5980a29957f3274c0e53d4db00822d0281f967
Marcus Wentz <52706599+MarcusWentz@users.noreply.github.com>
2022-08-09 18:41:39 -0400

    Update bytes32ToAddress.sol

--- contract/bytes32ToAddress.sol
+++ contract/bytes32ToAddress.sol
@@ -6,14 +6,7 @@ contract bytes32ToAddress {
     bytes32 public immutable addressBytes32TypeLeft  = 0xc1202e7d42655F23097476f6D48006fE56d38d4f000000000000000000000000; //TEST https://web3-type-converter.onbrn.com/
     address public addressAddresType;
 
-    function bytes32ToAddressTestShifted() public view returns (address) { //SHIFT 12 BYTES (96 BITS).
-        return address(uint160(uint256(addressBytes32TypeLeft>>96)));
-    }
-
-    function bytes32ToAddressConvertShifted() public { 
-        addressAddresType = address(uint160(uint256(addressBytes32TypeLeft>>96)));
-    }
-    
+    //BITS ALREADY IN PLACE, USES MUCH LESS GAS AND IS RECOMMENDED. 
     function bytes32ToAddressTest() public view returns (address) { //
         return address(uint160(uint256(addressBytes32TypeRight)));
     }
@@ -22,4 +15,13 @@ contract bytes32ToAddress {
         addressAddresType = address(uint160(uint256(addressBytes32TypeRight)));
     }
 
+    //USES MORE GAS SHIFTING! 
+    //AVOID SINCE IT USES MORE GAS IF POSSIBLE! 
+    function bytes32ToAddressTestShifted() public view returns (address) { //SHIFT 12 BYTES (96 BITS).
+        return address(uint160(uint256(addressBytes32TypeLeft>>96)));
+    }
+
+    function bytes32ToAddressConvertShifted() public { 
+        addressAddresType = address(uint160(uint256(addressBytes32TypeLeft>>96)));
+    }
 }
